8697097c4a637bd07f8133578173a7cc8bd506d6
Eric Schwartz <erics273@gmail.com>
2024-05-05 23:45:07 -0400

    cleaning up selector

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ window.onload = function () {
     let theForm = document.querySelector("#theForm");
 
     //get ALL the radios for coneOrCup using querySelectorAll
-    let coneOrCupRadios = document.querySelectorAll("#coneOrCup");
+    let coneOrCupRadios = document.querySelectorAll("input[name='coneOrCup']");
 
     theForm.addEventListener("submit", calcTotal);
 
@@ -19,15 +19,21 @@ window.onload = function () {
 function calcTotal(event) {
     event.preventDefault()
 
+    //get the the form data from the event target
     let theForm = event.target;
+
+    //get the number of scoops fron the form
     let numScoops = theForm.numScoops.value;
 
+    //They had to get at least one scoop so lets start our total at 2.25
     let iceCreamTotal = 2.25;
 
+    //if the number of scoops is > than 1 than we need to chare 1.35 for each of those
     if (numScoops > 1) {
         iceCreamTotal += (numScoops - 1) * 1.25;
     }
 
+    //if we sold a cup, we should need to calculate the toppings
     if (theForm.coneOrCup.value === "cup") {
 
         if (theForm.sprinkles.checked) {
@@ -47,6 +53,7 @@ function calcTotal(event) {
         }
     }
 
+    //calculate the tax assuming 7%
     let taxAmount = iceCreamTotal * (7 / 100);
 
     let message = `
@@ -55,10 +62,12 @@ function calcTotal(event) {
             <div>Total Due: $${(iceCreamTotal + taxAmount).toFixed(2)}</div>
         `
 
+    //add the total due to the element with the id of results
     document.querySelector("#results").innerHTML = message;
 
 }
 
+//function that hides or shows the toppings element on the page
 function hideShowToppings(event) {
 
     let toppingsSection = document.querySelector("#toppings");
